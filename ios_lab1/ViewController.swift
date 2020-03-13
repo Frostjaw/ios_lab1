2e2690dad979d315a1e2459ccfa2fc8de06f7eb3
RufinaRafikova <rafikova0rr@gmail.com>
2020-03-13 17:13:13 +0700

    Initial Commit

---
+++ ios_lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_lab1
+//
+//  Created by Разработчик on 13/03/2020.
+//  Copyright © 2020 Разработчик. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

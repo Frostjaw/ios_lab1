9a5ed4d78066cdadfea81598b07e538c66e26960
RufinaRafikova <rafikova0rr@gmail.com>
2020-03-13 19:48:35 +0700

    test

--- ios_lab1/ViewController.swift
+++ ios_lab1/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var playerImageView: UIImageView!
+    @IBOutlet weak var leftControlButton: UIButton!
+    @IBOutlet weak var rightControlButton: UIButton!
+    
+    var enemies: [[UIImageView]] = [[UIImageView]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        createEnemies()
+    }
+    @IBAction func leftButtonTouchDown(_ sender: Any) {
+        if (playerImageView.frame.origin.x >= 16){
+            playerImageView.frame.origin.x -= 10
+        }
+    }
+    
+    @IBAction func rightButtonTouchDown(_ sender: Any) {
+        if (playerImageView.frame.origin.x <= view.frame.maxX - playerImageView.frame.width - 16){
+            playerImageView.frame.origin.x += 10
+        }
+    }
+    
+    func createEnemies() {
+        for i in 0...2 {
+            var enemiesRow = [UIImageView]()
+            for j in 0...5{
+                let enemy = UIImageView(image: #imageLiteral(resourceName: "enemy"))
+                let enemyCGRect = CGRect(x: view.frame.width * (1/6) + CGFloat(j*50), y: view.frame.width * (1/7) + CGFloat(i*50), width: view.frame.width * (1/10), height: view.frame.width * (1/10))
+                enemy.frame = enemyCGRect
+                enemiesRow.append(enemy)
+                view.addSubview(enemy)
+            }
+            enemies.append(enemiesRow)
+        }
     }
 
 
